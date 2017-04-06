9840ff9ab0854ab32b409ef5b50ee4ee34707b05
joelfrewy <joelfrewin@gmail.com>
2017-04-06 23:53:59 +0800

    added wheels

--- RobotVR/Assets/Scripts/RobotBuilder.cs
+++ RobotVR/Assets/Scripts/RobotBuilder.cs
@@ -40,6 +40,9 @@ public class RobotBuilder: MonoBehaviour{
 		case "psd":
 			addPSD(args);
 			break;
+		case "wheel":
+			addWheels (args);
+			break;
 		default:
 			break;
 		}
@@ -54,6 +57,12 @@ public class RobotBuilder: MonoBehaviour{
 		GameObject model = OBJLoader.LoadOBJFile (modelpath);
 		model.transform.SetParent(robot.transform, false);
 		model.transform.rotation = Quaternion.Euler(0,90,0);
+		BoxCollider boxcollider = model.AddComponent<BoxCollider> ();
+		boxcollider.material = Resources.Load ("nofriction") as PhysicMaterial;
+		boxcollider.center = new Vector3 (0, 0.1f, 0);
+		boxcollider.size = new Vector3 (0.2f, 0.2f, 0.2f);
+		model.AddComponent<Rigidbody> ();
+		model.layer = 9;
 	}
 
 	void addPSD(string[] args){
@@ -64,6 +73,22 @@ public class RobotBuilder: MonoBehaviour{
 		psd.transform.position = new Vector3 (-1*float.Parse(args[4])/1000,float.Parse(args[5])/1000,float.Parse(args[3])/1000);
 		psd.transform.rotation = Quaternion.Euler (0, -1*float.Parse(args[6]), 0);
 		psd.transform.SetParent (robot.transform, false);
+	}
 
+	void addWheels(string[] args){
+		addWheel (float.Parse (args [1]) / 1000f, float.Parse (args [4]) / 1000f, -90f);
+		addWheel (float.Parse (args [1]) / 1000f, float.Parse (args [4]) / -1000f, -90f);
+	}
+
+	void addWheel(float diameter, float distance, float rotation){
+		GameObject wheel = Instantiate (Resources.Load("Cylinder")) as GameObject;
+		wheel.transform.SetParent (robot.transform, false);
+		wheel.transform.localScale = new Vector3 (diameter, 0.01f, diameter);
+		wheel.transform.position = new Vector3 (distance, diameter/2, 0);
+		wheel.transform.rotation = Quaternion.Euler (0, 0, rotation);
+		wheel.layer = 8;
+		HingeJoint hinge = wheel.AddComponent<HingeJoint> ();
+		hinge.axis = new Vector3 (0, 1, 0);
+		hinge.connectedBody = robot.transform.GetChild(0).GetComponent<Rigidbody>();
 	}
 }
