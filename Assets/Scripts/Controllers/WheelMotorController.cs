7605277094748e9db45ef8ebc7e9799b3a60571a
Travis <tgpovey@gmail.com>
2017-05-04 16:30:59 +0800

    Fixed scene fiile

--- Assets/Scripts/Controllers/WheelMotorController.cs
+++ Assets/Scripts/Controllers/WheelMotorController.cs
@@ -22,33 +22,33 @@ public class Pose
 
 public class WheelMotorController : MonoBehaviour
 {
-    public List<Wheel> wheels;
+    public List<Wheel> wheels; // the information about each individual wheel  
     public float maxMotorTorque; // maximum torque the motor can apply to wheel
     public float maxSteeringAngle; // maximum steer angle the wheel can have
-	public float wheelDist;
+    public float wheelDist;
     public float Rot;
-	public float w;
-	public Vector3 Pos;
-	public float v;
+    public float w;
+    public Vector3 Pos;
+    public float v;
 
-	public bool drive = false; // for testing purposes
+    public bool drive = false; // for testing purposes
 
-	//vw parameters
-	public float targetDist;
-	public float travelledDist;
-	public float targetRot;
-	public float travelledRot;
-	public string checkType;
-	public bool checkActive;
+    //vw parameters
+    public float targetDist;
+    public float travelledDist;
+    public float targetRot;
+    public float travelledRot;
+    public string checkType;
+    public bool checkActive;
 
     public Action DriveDoneDelegate;
-    
-    private void Start()
+
+    private void Awake()
     {
        // wheels = new List<Wheel>();
-		Pos = new Vector3(0,0,0);
+        Pos = new Vector3();
     }
-    
+
     // Set the local PID Parameters
     public void SetPIDParams(int motor, int p, int i, int d)
     {
@@ -63,8 +63,7 @@ public class WheelMotorController : MonoBehaviour
     // Set the speed of a single motor (controlled)
     public void SetMotorControlled(int motor, int ticks)
     {
-        
-        if(wheels[motor].pidEnabled)
+        if (!wheels[motor].pidEnabled)
         {
             SetPIDParams(motor, 4, 1, 1);
         }
@@ -72,77 +71,92 @@ public class WheelMotorController : MonoBehaviour
         SetMotorSpeed(motor, ticks);
     }
     // Update visual of wheel on each frame
-    private void Update()
+    private void FixedUpdate()
     {
-        updatePosition ();
-		checkDrive ();
-	}
+        if (drive)
+        { //for testing purposes
+            DriveCurve(1, 90, 0.1f);
+            drive = false;
+        }
 
-	public void DriveStraight(float distance, float velocity){
-		resetController ();
-		targetDist = distance;
-		checkType = "distance";
-		SetSpeed (velocity, 0);
-		checkActive = true;
-	}
+        updatePosition();
+        checkDrive();
+    }
 
-	public void DriveTurn(float rotation, float velocity){
-		resetController ();
-		targetRot = rotation;
-		checkType = "rotation";
-		SetSpeed (0, velocity);
-		checkActive = true;
-	}
+    public void DriveStraight(float distance, float velocity)
+    {
+        resetController();
+        targetDist = distance;
+        checkType = "distance";
+        SetSpeed(velocity, 0);
+        checkActive = true;
+    }
 
-	public void DriveCurve(float distance, float rotation, float velocity){
-		resetController ();
-		targetDist = distance;
-		checkType = "distance";
-		SetSpeed(velocity, (float)velocity / distance * rotation); //finds w via ratio
-		checkActive = true;
-	}
+    public void DriveTurn(float rotation, float velocity)
+    {
+        resetController();
+        targetRot = rotation;
+        checkType = "rotation";
+        SetSpeed(0, velocity);
+        checkActive = true;
+    }
 
-	public int DriveDone(){
-		if (!checkActive)
-			return 1;
+    public void DriveCurve(float distance, float rotation, float velocity)
+    {
+        resetController();
+        targetDist = distance;
+        checkType = "distance";
+        SetSpeed(velocity, (float)velocity / distance * rotation); //finds w via ratio
+        checkActive = true;
+    }
 
-		//code for stalling or blockage, return 4
+    public int DriveDone()
+    {
+        if (!checkActive)
+            return 1;
 
-		return 0;
-	}
+        //code for stalling or blockage, return 4
 
-	//set translational and rotational target velocities
-	public void SetSpeed (float setv, float setw){
-		wheels[0].SetSpeed (setv + setw * wheelDist / 2 * Mathf.Deg2Rad);
-		wheels[1].SetSpeed (setv - setw * wheelDist / 2 * Mathf.Deg2Rad);
-	}
+        return 0;
+    }
 
-	private void updatePosition(){
-		float lspeed = wheels[0].GetSpeed ();
-		float rspeed = wheels[1].GetSpeed ();
-		float newv = (rspeed + lspeed) / 2;
-		float neww = (lspeed - rspeed) / wheelDist * Mathf.Rad2Deg;
-		Pos.z += ((newv + v) / 2) * Mathf.Cos (Mathf.Deg2Rad * Rot);
-		Pos.x += ((newv + v) / 2) * Mathf.Sin (Mathf.Deg2Rad * Rot);
-		Rot += (neww + w) / 2;
+    //set translational and rotational target velocities
+    public void SetSpeed(float setv, float setw)
+    {
+        wheels[0].SetSpeed(setv + setw * wheelDist / 2 * Mathf.Deg2Rad);
+        wheels[1].SetSpeed(setv - setw * wheelDist / 2 * Mathf.Deg2Rad);
+    }
 
-		while (Rot > 180) {
-			Rot -= 360;
-		}
-		while (Rot < -180) {
-			Rot += 360;
-		}
+    private void updatePosition()
+    {
+        float lspeed = wheels[0].GetSpeed();
+        float rspeed = wheels[1].GetSpeed();
+        float newv = (rspeed + lspeed) / 2;
+        float neww = (lspeed - rspeed) / wheelDist * Mathf.Rad2Deg;
+        Pos.z += ((newv + v) / 2) * Mathf.Cos(Mathf.Deg2Rad * Rot);
+        Pos.x += ((newv + v) / 2) * Mathf.Sin(Mathf.Deg2Rad * Rot);
+        Rot += (neww + w) / 2;
 
-		v = newv;
-		w = neww;
-	}
+        while (Rot > 180)
+        {
+            Rot -= 360;
+        }
+        while (Rot < -180)
+        {
+            Rot += 360;
+        }
 
-	private void resetController(){
-		targetDist = 0;
-		targetRot = 0;
-		travelledDist = 0;
-		travelledRot = 0;
-	}
+        v = newv;
+        w = neww;
+    }
+
+    private void resetController()
+    {
+        targetDist = 0;
+        targetRot = 0;
+        travelledDist = 0;
+        travelledRot = 0;
+    }
 
     public void TestDelegation()
     {
@@ -152,32 +166,36 @@ public class WheelMotorController : MonoBehaviour
         }
     }
 
-	private void checkDrive(){
-		if (!checkActive)
-			return;
+    private void checkDrive()
+    {
+        if (!checkActive)
+            return;
 
-		switch (checkType) {
-		case "distance":
-			travelledDist += v;
-			if (Mathf.Sign(targetDist) * (targetDist - travelledDist) > 0)
-				return;
-			break;
-		case "rotation":
-			travelledRot += w;
-			if (Mathf.Sign(targetRot) * (targetRot - travelledRot) > 0)
-				return;
-			break;
-		default:
-			break;
-		}
+        switch (checkType)
+        {
+            case "distance":
+                travelledDist += v;
+                if (Mathf.Sign(targetDist) * (targetDist - travelledDist) > 0)
+                    return;
+                break;
+            case "rotation":
+                travelledRot += w;
+                if (Mathf.Sign(targetRot) * (targetRot - travelledRot) > 0)
+                    return;
+                break;
+            default:
+                break;
+        }
 
-		//journey complete
-		SetSpeed(0, 0);
-		checkActive = false; 
-		resetController ();
+        //journey complete
+        SetSpeed(0, 0);
+        checkActive = false;
+        resetController();
+        Debug.Log("Drive is completed!");
         if (DriveDoneDelegate != null)
         {
+            Debug.Log("Calling delegate chain");
             DriveDoneDelegate();
         }
-	}
+    }
 }
